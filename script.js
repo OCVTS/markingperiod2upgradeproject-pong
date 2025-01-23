3015820ffcd8e3df000983db736108f37d33a6c4
Daivion Hardy <38371@ocvts.org>
2025-01-23 15:51:25 +0000

    starter code

---
+++ script.js
@@ -0,0 +1,107 @@
+//player variables
+let p1X = 20;
+let p1Y = 250;
+let p2X = 780;
+let p2Y = 250;
+let pWidth = 20;
+let pHeight = 100;
+let p1Speed = 5;
+let p2Speed = 5;
+
+//ball variables
+let ballX = 400;
+let ballY = 250;
+let ballSize = 20;
+let ballSpeed = 2;
+let ballDirectionX = 1;
+let ballDirectionY = 1;
+
+//scoreboard variables
+let p1Score = 0;
+let p2Score = 0;
+
+function setup() {
+    createCanvas(800,500);
+    rectMode(CENTER);
+    textAlign(CENTER);
+    // background(100);
+}// close setup
+
+function draw(){
+keyPressed();
+keyTyped();
+
+    // draw court
+    background(0);
+    noFill();
+    stroke(255);
+    strokeWeight(3);
+    line(400,0,400,500);
+    rect(400,250,800,500);
+
+    //draw players
+    noStroke();
+    fill(255);
+    rect(p1X, p1Y, pWidth, pHeight)
+    rect(p2X, p2Y, pWidth, pHeight)
+
+// draw ball
+noStroke();
+fill(255);
+square(ballX, ballY, ballSize)
+
+//draw score board
+noStroke();
+fill(255);
+textSize(30);
+text(p1Score, 350, 50);
+text(p2Score,450,50);
+
+// move ball
+ballX = ballX + (ballSpeed * ballDirectionX);
+ballY = ballY + (ballSpeed * ballDirectionY);
+
+// collide with top / bottom walls
+if (ballY + ballSize/2 >= 500) {
+    ballDirectionY = ballDirectionY * -1;
+}
+if (ballY - ballSize/2 <= 0) {
+    ballDirectionY = ballDirectionY * -1;
+}
+
+
+
+
+// collide with players
+if (ballX >= p2X - pWidth/2 && ballX <= p2X + pWidth /
+    2 && ballY >= p2Y - pHeight/2 && ballY <= p2Y + pHeight/2)
+    {
+        ballDirectionX = ballDirectionX * -1;
+    }
+    if (ballX >= p1X - pWidth/2 && ballX <= p1X + pWidth /
+        2 && ballY >= p1Y - pHeight/2 && ballY <= p1Y + pHeight/2)
+        {
+            ballDirectionX = ballDirectionX * -1;
+        }
+    
+
+
+}//close draw
+
+function keyPressed() {
+    if (keyCode == UP_ARROW && keyIsPressed == true){
+        p2Y = p2Y - p2Speed;
+    }
+    if (keyCode == DOWN_ARROW && keyIsPressed == true) {
+        p2Y = p2Y + p2Speed;
+    }
+}// close keypressed
+
+function keyTyped() {
+if (key == 'w' && keyIsPressed == true) {
+    p1Y = p1Y - p1Speed;
+}
+if (key == 's' && keyIsPressed == true) {
+    p1Y = p1Y + p1Speed;
+}
+}//close keyTyped
